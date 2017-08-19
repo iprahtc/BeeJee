9227cdf323ff06c8bbc1fd7401e64fec08b76af3
iprahtc <ipra-8@mail.ru>
2017-08-19 16:40:51 +0300

    Связь с БД

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -1,1 +1,61 @@
-<h1>Добро пожаловать!</h1>
+<?php print_r($data)?>
+<div class="row">
+    <div class="col s12 m3">
+        <div class="card grey light-green lighten-5">
+            <h5 class="center-align">Сортировка</h5>
+            <div class="card-content white-text">
+                <div class="row">
+                    <form action="#">
+                        <p>
+                            <input name="group1" type="radio" id="test1" />
+                            <label for="email">email</label>
+                        </p>
+                        <p>
+                            <input name="group1" type="radio" id="test2" />
+                            <label for="test2">Имя пользователя</label>
+                        </p>
+                        <p>
+                            <input name="group1" type="radio" id="test3"  />
+                            <label for="test3">Статус</label>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col s12 m9">
+        <div class="row">
+            <?php foreach($data as $v){?>
+            <div class="col s12 m12">
+                <div class="card blue-grey darken-1">
+                    <div class="card-content white-text">
+                        <div class="row">
+                            <div class="col s4">
+                                <img class="activator" <?php echo "src='images/". $v['img']."'"?>/>
+                            </div>
+                            <div class="col s6">
+                                <h6>Имя пользвателя: <?php echo $v['name'] ?></h6>
+                                <h6>Email: <?php echo $v['email'] ?></h6>
+                                <h6>Статус: <?php echo $v['status'] ?></h6>
+                            </div>
+                        </div>
+                        <span class="card-title"><?php echo $v['name_text'] ?></span>
+                        <p><?php echo $v['text'] ?></p>
+                    </div>
+                </div>
+            </div>
+            <?php } ?>
+        </div>
+    </div>
+    <div class="row center">
+        <ul class="pagination">
+            <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+            <li class="active"><a href="#!">1</a></li>
+            <li class="waves-effect"><a href="#!">2</a></li>
+            <li class="waves-effect"><a href="#!">3</a></li>
+            <li class="waves-effect"><a href="#!">4</a></li>
+            <li class="waves-effect"><a href="#!">5</a></li>
+            <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+        </ul>
+    </div>
+</div>
--- application/views/template_view.php
+++ application/views/template_view.php
@@ -1,17 +1,5 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<!--
-Design by Free CSS Templates
-http://www.freecsstemplates.org
-Released for free under a Creative Commons Attribution 3.0 License
-
-Name       : Accumen
-Description: A two-column, fixed-width design with dark color scheme.
-Version    : 1.0
-Released   : 20120712
-
-Modified by VitalySwipe
--->
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE>
+<html>
 	<head>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<meta name="description" content="" />
@@ -20,10 +8,10 @@ Modified by VitalySwipe
 
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 		<link rel="stylesheet" type="text/css" href="/css/style.css" />
-		<link rel="stylesheet" type="text/css" href="/css/materialize.min.css" />
+		<link rel="stylesheet" type="text/css" href="/css/materialize.css" />
 
-		<script src="/js/jquery-1.6.2.js" type="text/javascript"></script>
-		<script src="/js/materialize.min.js" type="text/javascript"></script>
+        <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+		<script src="/js/materialize.js" type="text/javascript"></script>
 		<script type="text/javascript">
 		$(document).ready(function() { 
 
@@ -35,12 +23,14 @@ Modified by VitalySwipe
             <div class="nav-wrapper container">
                 <a href="#" class="brand-logo"><img src="/images/Screenshot.png"></a>
                 <ul id="nav-mobile" class="right hide-on-med-and-down">
-                    <li><a href="#">Sass</a></li>
-                    <li><a href="#">Components</a></li>
-                    <li><a href="#">JavaScript</a></li>
+                    <li><a href="#">Главная</a></li>
+                    <li><a href="#">+Новая задача</a></li>
+                    <li><a href="#">Вход</a></li>
                 </ul>
             </div>
         </nav>
-        <?php include 'application/views/'.$content_view; ?>
+        <div class="content">
+            <?php include 'application/views/'.$content_view; ?>
+        </div>
 	</body>
 </html>
